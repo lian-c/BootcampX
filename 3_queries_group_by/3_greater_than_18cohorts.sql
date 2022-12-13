e6ddafd3a8e641d7527ae339cb8f6c34866336e0
lian-c <lian.chan@telus.com>
2022-12-12 20:03:48 -0800

    query for over 18 students grouped by cohort

---
+++ 3_queries_group_by/3_greater_than_18cohorts.sql
@@ -0,0 +1,13 @@
+SELECT cohorts.name as cohort_name, COUNT(students.id) as student_count
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohorts.name
+HAVING COUNT(students.id) >= 18
+ORDER BY COUNT(students.id);
+
+-- alternative SELECT cohorts.name as cohort_name, count(students.*) AS student_count 
+-- FROM cohorts
+-- JOIN students ON cohorts.id = cohort_id
+-- GROUP BY cohort_name 
+-- HAVING count(students.*) >= 18
+-- ORDER BY student_count;
