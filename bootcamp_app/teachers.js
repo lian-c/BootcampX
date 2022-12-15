04d60313df7ba7d5aeead77413389a846d794a6d
lian-c <lian.chan@telus.com>
2022-12-14 19:28:17 -0800

    Edit both js files to parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,24 +1,24 @@
 const { Pool } = require('pg');
 const args = process.argv
 
-const [node, file, cohortName, max] = args
-
+const cohortName = process.argv[2];
+const values = [cohortName]
 const pool = new Pool({
   user: 'T945100',
   password: '123',
   host: 'localhost',
   database: 'bootcampx'
 });
-
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM assistance_requests
 INNER JOIN teachers ON teachers.id = teacher_id
 INNER JOIN students ON students.id = student_id
 INNER JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${cohortName}'
+WHERE cohorts.name like $1
 ORDER BY teacher;
-`)
+`
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
